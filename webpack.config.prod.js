decd92f8f707a0752c2f358a1f33c55a115425af
Mark Wirblich <mark@wirblich.com>
2020-12-02 12:39:44 +0100

    test

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -26,12 +26,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        use: ['babel-loader'],
       },
       {
         test: /\.scss$/,
