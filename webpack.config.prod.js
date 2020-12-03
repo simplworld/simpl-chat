06601230bd605ccff53122e69607056f8ffab89d
Mark Wirblich <mark@wirblich.com>
2020-12-03 15:21:21 +0100

    Readded TerserPlugin
    Added github workflow
    
    Testing workflow
    
    hmmm
    
    test
    
    Cleaned up

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,115 +1,101 @@
 'use strict'
-const pkg = require('./package.json');
 
-const webpack = require('webpack');
 const path = require('path');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const autoprefixer = require('autoprefixer');
+const pkg = require('./package.json');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2',
-        library: pkg.name,
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: pkg.name,
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
     },
-    resolve: {
-        extensions: ['.jsx', '.js']
-    },
-    externals: {
-        'react': {
-            commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'React',
-            root: 'React'
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'ReactDOM',
-            root: 'ReactDOM'
-        }
-    },
-    target: 'web',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                        // options: { hmr: true }
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 2,
-                            sourceMap: true,
-                            modules: {
-                                localIdentName: '[name]__[local]___[hash:base64:5]',
-                            }
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: () => [
-                                require('postcss-flexbugs-fixes'), // eslint-disable-line
-                                autoprefixer({
-                                    flexbox: 'no-2009'
-                                })
-                            ]
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        // options: {
-                        //   includePaths: [path.resolve(__dirname, 'src/scss')]
-                        // }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        esModule: false,
-                    },
-                }
+  },
+  target: 'web',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: true,
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              }
             }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'), // eslint-disable-line
+                autoprefixer({
+                  flexbox: 'no-2009'
+                })
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader',
+          }
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'styles.css',
-            chunkFileName: '[id].css'
-        })
-    ],
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true
-            }),
-            new OptimizeCSSAssetsPlugin({})
-        ]
-    }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            esModule: false,
+          },
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFileName: '[id].css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
 };
